847fc68b624ef935d38eed6fcc1665f69fdb850f
Philipp Kyeck <philipp@beta-interactive.de>
2014-02-24 17:13:58 +0100

    first version of docs

---
+++ api_project.js
@@ -0,0 +1,10 @@
+define({
+  "name": "easy-ust-api",
+  "version": "0.0.1",
+  "description": "API for easy ust web service",
+  "apidoc": "",
+  "generator": {
+    "version": "0.3.0",
+    "time": "2014-02-24T16:01:32.039Z"
+  }
+});
